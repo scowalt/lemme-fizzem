78b9e24da823bb527053855a83d992b6ffed5427
Scott Walters <srwalte2@illinois.edu>
2014-01-21 15:15:11 -0600

    This may or may not be finished

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import re
-import win32api
 import shutil
+import sys
+import win32api
 
-CHAMPION = "Fizz"
+DIRECTORY_REGEX = r"RADS\\projects\\lol_air_client\\releases\\(?P<version>[0-9\.]+?)\\deploy\\assets\\sounds\\(?P<language>[a-z]{2}\_[A-Z]{2}?)\\[Cc]hampions"
+BAD_REGEX = r"\$RECYCLE.BIN"
 
 def replace_sounds_with_champ(root_folder, champion):
 	rex = re.compile(r"^.*%s\.mp3\.bak"%CHAMPION)
@@ -12,7 +14,10 @@ def replace_sounds_with_champ(root_folder, champion):
 		for f in files:
 			result = rex.search(f)
 			if result:
-				print "Found Fizz! " + os.path.join(root, f)
+				champion_file = os.path.join(root, f)
+				print "Found Fizz! " + champion_file
+				break
+
 	if not champion_file:
 		return
 	
@@ -43,19 +48,24 @@ def backup_files(root_folder):
 	for root,dirs,files in os.walk(root_folder):
 		for f in files:
 			backed_up = rex.search(f)
-			if not backed_up
+			if not backed_up:
 				new_f = f + ".bak"
 				os.rename(os.path.join(root,f), os.path.join(root, new_f))
 
 # http://stackoverflow.com/a/13068033/1222411
 def find_dir(root_folder, rex):
+	bad = re.compile(BAD_REGEX)
 	for root,dirs,files in os.walk(root_folder):
 		for d in dirs:
 			full_dir = os.path.join(root,d)
 			result = rex.search(full_dir)
-			if result:
-				backup_files(full_dir)
-				replace_sounds_with_champ(full_dir, CHAMPION)
+			bad_directory = bad.search(full_dir)
+			if result and not bad_directory:
+				if sys.argv[1] is 'restore':
+					restore_files(root_folder)
+				else:
+					backup_files(full_dir)
+					replace_sounds_with_champ(full_dir, sys.argv[1])
 
 
 # http://stackoverflow.com/a/13068033/1222411
@@ -67,5 +77,8 @@ def find_dir_in_all_drives(dir_name):
 		print "Scanning drive " + str(drive) + " for League of Legends"
 	 	find_dir( drive, rex )
 
-# http://stackoverflow.com/a/9269316/1222411
-find_dir_in_all_drives(r"lol_air_client\\releases\\(?P<version>[0-9\.]+?)\\deploy\\assets\\sounds\\(?P<language>[a-z]{2}\_[A-Z]{2}?)\\[Cc]hampions")
+if len(sys.argv) is not 2:
+	print "Wrong arguements"
+else:
+	# http://stackoverflow.com/a/9269316/1222411
+	find_dir_in_all_drives(DIRECTORY_REGEX)
