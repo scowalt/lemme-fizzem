2805b7928950c0378f1ffa0a8011806b809a1ca2
Scott Walters <srwalte2@illinois.edu>
2014-01-21 12:25:27 -0600

    Added directory scanning for proper files
    Regex needs work, but adding a second match group for the region doesn't
    work for some reason

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print "Hello World!"
+import os
+import re
+import win32api
+
+# http://stackoverflow.com/a/13068033/1222411
+def find_dir(root_folder, rex):
+	for root,dirs,files in os.walk(root_folder):
+		for d in dirs:
+			full_dir = root + d
+			result = rex.search(full_dir)
+			if result:
+				print os.path.join(root,d)
+
+# http://stackoverflow.com/a/13068033/1222411
+def find_dir_in_all_drives(dir_name):
+	#create a regular expression for the file
+	rex = re.compile(dir_name)
+	for drive in win32api.GetLogicalDriveStrings().split('\000')[:-1]:
+		print "Scanning drive " + str(drive)
+		find_dir( drive, rex )
+
+# http://stackoverflow.com/a/9269316/1222411
+find_dir_in_all_drives(r"lol_air_client\\releases\\(?P<version>[0-9\.]+?)\\deploy\\assets\\sounds\\")
